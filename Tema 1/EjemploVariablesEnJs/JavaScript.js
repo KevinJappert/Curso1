7062c45feba00d720bb15d05f476cb4aca099cbd
KevinJappert <jappertkevin@gmail.com>
2022-04-07 19:25:17 -0300

    push

---
+++ Tema 1/EjemploVariablesEnJs/JavaScript.js
@@ -0,0 +1,10 @@
+var userName; //Declaración de variable
+userName = "Juan Gabriel Gomila"; //Inicialización = La primera asignación
+document.write("Este programa me lo ha enseñado a hacer "+userName);
+
+var age = 29; //Declaración e inicialización en una sola instrucción;
+document.write("Ahora mismo tiene "+age+" años.");
+
+age = 30;
+document.write("El 19 de Mayo cumplirá "+age);
+console.log(age);
