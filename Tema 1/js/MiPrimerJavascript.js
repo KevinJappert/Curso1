c99cf5d729fab5c274f0045ec6f03387fe76326a
KevinJappert <jappertkevin@gmail.com>
2022-04-06 21:57:58 -0300

    push

---
+++ Tema 1/js/MiPrimerJavascript.js
@@ -0,0 +1,15 @@
+function sayHello(){
+    document.write("<p>Hello, Javascript</p>");
+    linebreak();
+}
+
+function linebreak(){
+    document.write("<br/>");
+}
+
+document.write("<strong>Hola, esto es Javascript con JB</strong><br/>");
+document.write("<em>Este es mi primer programa en JS</em><br/>");
+			
+sayHello();
+sayHello();
+sayHello();
