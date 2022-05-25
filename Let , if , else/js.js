ef3cc356f376e8b4212b63df4ee85c8bca72af21
KevinJappert <jappertkevin@gmail.com>
2022-05-24 22:28:21 -0300

    if else

---
+++ Let , if , else/js.js
@@ -0,0 +1,30 @@
+// let password = "pepe123"
+
+//let input = "pepe123asdasd"
+
+//let result = input == password;
+
+//if (result == true) {
+//    console.log("Logeaste correctamente");
+//} else{
+//    console.log("Logeaste incorrectamente ");  
+//} 
+
+
+
+let score = 70;
+
+
+
+if (score > 30 ) {
+
+    console.log("Necesitas practicar mas")
+
+} else if (score <= 30 ) {
+
+    console.log("Estas mejorando")
+}
+ if (score < 10 ) {
+    console.log("Necesitas seguir un tutorial")
+}
+
