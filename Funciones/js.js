8ac704b626ea580ae0aede1c6351e20fedc17112
KevinJappert <jappertkevin@gmail.com>
2022-05-26 17:49:06 -0300

    funciones

---
+++ Funciones/js.js
@@ -0,0 +1,19 @@
+function saludar(name) {
+    console.log("Hola " + name);
+
+}
+
+saludar("Marcos");
+saludar("Kevin");
+saludar("Lucas");
+
+
+function sumar(n1 , n2) {
+    console.log(n1 + n2);
+
+}
+
+sumar(3,5);
+sumar(343,500);
+sumar(3 ,  "te meti");
+sumar(3,5);
