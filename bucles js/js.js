10dca1e370fae294f73ade828677a43e810b03dd
KevinJappert <jappertkevin@gmail.com>
2022-07-21 16:46:54 -0300

    Bucles

--- bucles js/js.js
+++ bucles js/js.js
@@ -38,9 +38,46 @@
 //bucle reproduciendo al reves o normal.
 
 
-let names = ["ryan", "joe" ,"john"];
+// let names = ["ryan", "joe" ,"john"];
 
-for(let i = names.length - 1; i >= 0; i--) {
-    console.log(names[i]);
-}
+// for(let i = names.length - 1; i >= 0; i--) {
+//     console.log(names[i]);
+// }
 
+//while// ejecuta el codigo y vuelve a preguntar
+
+// do {
+
+// } while (condition); primero ejecuta el codigo y dsp pregunta una vez nomas
+
+//break //hace que termine la secuencia
+
+
+// for (let i = 0; i <=6 ; i++) {
+//     console.log(i)
+// }
+
+
+
+// for (let i = 6; i >=0 ; i--) {
+    
+//     // if(i==3){
+//     //     continue
+//     // }
+//     // if(i==3){
+//     //     break
+//     // }
+    
+//     console.log(i);  
+// }
+
+ 
+//  let animales = ["gato", "perro", "loro"]  //muestra posicion el in
+// for (animal in animales) {
+//     console.log(animal);
+// }
+
+
+// for (animal of animales) {    //muestra valor el of
+//     console.log(animal);
+// }
