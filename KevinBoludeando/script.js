5aef8a436598841d834044acf107a4d3c6f3920c
KevinJappert <jappertkevin@gmail.com>
2022-08-02 16:14:18 -0300

    psuh

---
+++ KevinBoludeando/script.js
@@ -0,0 +1,21 @@
+let sumar = (num1,num2)=>{
+    return num1 + num2;
+}
+
+let restar = (num1,num2)=>{
+    return num1 - num2;
+}
+
+let dividir = (num1,num2)=>{
+    return num1 / num2;
+}
+
+let multiplicar = (num1,num2)=>{
+    return num1 * num2;
+}
+
+
+console.log(sumar(4,5))
+console.log(restar(4,5))
+console.log(dividir(20,5))
+console.log(multiplicar(4,10))
